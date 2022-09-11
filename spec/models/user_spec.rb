36bc2c48d70b185efb97358e8522b161e96e3f0d
Lucas Falbo <95.lucas@gmail.com>
2022-09-11 11:09:03 -0300

    Add new user model test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,22 +4,27 @@ RSpec.describe User, type: :model do
   context ".create" do
 
     context "validation:" do
+      let(:name) {"Armand"}
+      let(:email) { Faker::Internet.email }
+      let(:password) { Faker::Alphanumeric.alpha(number: 6) }
 
       describe "creating user with valid info" do
 
         it "should create a valid user" do
 
-          user = User.new(name: "Test", email: "test@test.com", password: "123456")
+          user = described_class.new(name:,email:, password:)
           expect(user).to be_valid
 
         end
 
         it "should not be an admin by default" do
-          user = User.new(name: "Test", email: "test@test.com", password: "123456")
+          user = described_class.new(name:, email:, password:)
           expect(user.admin?).to be false
         end
 
       end
+
+    describe "creating user with invalid info"
 
     end
 
