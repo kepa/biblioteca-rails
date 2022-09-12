721f185c660cb48b3d96604be33934d56b784e80
Lucas Falbo <95.lucas@gmail.com>
2022-09-12 10:45:15 -0300

    Create new Books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
