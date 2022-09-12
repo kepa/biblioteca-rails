10fd3dbdab79b28f099e39b2d6301562c864388a
Lucas Falbo <95.lucas@gmail.com>
2022-09-12 17:46:25 -0300

    Add new testing methods for rental#finalize

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Rental, type: :model do
         it "should create a new rental" do
           rental = user.rentals.create(book_id: book.id)
           expect(rental.active).to be true
-          #expect(rental.checkout_date.day).to eql(Time.now.day)
+          expect(rental.checkout_date.day).to eql(Time.now.utc.day)
           expect(Book.checked_out?(book.id)).to be true
         end
       end
@@ -19,6 +19,35 @@ RSpec.describe Rental, type: :model do
 
   end
 
+  context "#finalize" do
 
+    describe "finalizing a rental:" do
+      let(:user) { create(:user) }
+      let(:book_checked_out) { create(:book, :checked_out) }
+      let(:rental) { create(:rental, book_id:book_checked_out.id, user_id: user.id) }
+
+      before do
+        user.rentals << rental
+      end
+
+      it "should turn inactive a user's rental" do
+        rental.finalize
+        expect(user.rentals.first.active).to be false
+      end
+
+      it "should register check-in date" do
+        rental.finalize
+        expect(rental.checkin_date.day).to eql(Time.now.utc.day)
+      end
+
+      it "should set book's checkout_status" do
+        rental.finalize
+        rental.save
+        expect(Book.checked_out?(book_checked_out.id)).to be false
+      end
+
+    end
+
+  end
 
 end
