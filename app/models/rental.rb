6c9c6fea81a643f369ca66ee6fd76d43ce536535
Lucas Falbo <95.lucas@gmail.com>
2022-09-15 09:25:39 -0300

    Add comments to better document changes

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,6 +3,7 @@
 class Rental < ApplicationRecord
   belongs_to :user
 
+  # Using Activerecord scopes for filtering
   scope :filter_by_status, ->(status) { where active: status }
   scope :filter_by_checkout, lambda { |init_date, end_date|
                                where('checkout_date >= ? and checkout_date < ?', init_date, end_date)
@@ -12,6 +13,7 @@ class Rental < ApplicationRecord
 
   after_destroy :revert_checkout
 
+  #Initial setup
   def init_check_out_book
     self.active = true
     self.checkout_date = Time.now
@@ -20,12 +22,14 @@ class Rental < ApplicationRecord
     self.book_title = book.title
   end
 
+  #When rental is done, reverts the book availability
   def finalize
     self.active = false
     self.checkin_date = Time.now
     Book.find(book_id).toggle_check_out
   end
 
+  #On destroy reverts book's checkout status
   def revert_checkout
     Book.find(book_id).toggle_check_out
   end
