867f31c9c9cc11b184c06ee2a9e67e9fd32f3427
Lucas Falbo <95.lucas@gmail.com>
2022-09-15 00:03:00 -0300

    Add simple search by title to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   validates_presence_of :title,:author,:category
   validates_uniqueness_of :title
 
+  scope :filter_by_title, -> (title) {where("title like ?", "#{title}%")}
   scope :filter_by_author, -> (author) {where("author like ?", "#{author}%")}
   scope :filter_by_category, -> (category) {where category: category}
   scope :filter_by_status, -> (status) {where check_out: status}
